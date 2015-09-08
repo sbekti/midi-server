aba0dbd0575e781ed0ad6cc47d8fa9dbf10a0f1d
Samudra Harapan Bekti <samudra.bekti@gmail.com>
2015-09-08 00:49:43 -0700

    Fix socket

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var Promise = require('promise');
 var midi = require('midi-node');
+var request = require('request');
 
 function createClient(port, address) {
   return new Promise(function(resolve, reject) {
@@ -28,25 +29,29 @@ app.get('/', function(req, res) {
 });
 
 app.post('/midi/register', function(req, res) {
-  var body = req.body.toString('utf8');
-  var tokens = body.split(':');
+  var body = req.body;
+  var bodyString = body.toString('utf8');
+  console.log('bodyString: ' + bodyString);
+  var tokens = bodyString.split(':');
+  console.log('tokens: ' + tokens);
   var address = tokens[0];
   var port = parseInt(tokens[1]);
+  console.log('tokens: ' + tokens);
 
   createClient(port, address).then(function(client) {
-    // midiWriter = new midi.Writer(client);
-    // midiWriter.startFile(0, 1, 128);
-    // midiWriter.startTrack();
-    // midiWriter.noteOff(0, 0, 80, 100);
-    // midiWriter.endOfTrack(0);
-    res.send(body);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+      // midiWriter = new midi.Writer(client);
+      // midiWriter.startFile(0, 1, 128);
+      // midiWriter.startTrack();
+      // midiWriter.noteOff(0, 0, 80, 100);
+      // midiWriter.endOfTrack(0);
+      res.send(body);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(8080, function() {
   var host = server.address().address;
   var port = server.address().port;
 
